6e0ea2af9301cda7ced0db67feb95406151956f5
dlathait <dlathait@gmail.com>
2026-03-26 15:23:19 +0530

    first time push

---
+++ src/main/java/org/GitSample1/GitReg.java
@@ -0,0 +1,17 @@
+package org.GitSample1;
+
+public class GitReg {
+
+	public void method1()
+	{
+		System.out.println("method1");
+	}
+	public void method2()
+	{
+		System.out.println("method2");
+	}
+	public void method3()
+	{
+		System.out.println("method3");
+	}
+}
